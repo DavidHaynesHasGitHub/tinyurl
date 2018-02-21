10f78aa2b97e16cb6c44ac85b432b0a13921fafd
David Haynes <DavidHaynesVictoria@gmail.com>
2018-02-21 09:09:09 -0800

    fixed server.js to work as required

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.set("view engine", "ejs");
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,21 +21,19 @@ app.get("/urls/new", (req, res) => {
   res.render("pages/urls_new");
 });
 
-app.get("/urls/:id", (req, res) => {
-  let templateVars = {shortURL: urlDatabase};
-  res.render("pages/urls_show", templateVars);
+app.get("/urls/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+    console.log(shortURL)
+  let longURL = urlDatabase[shortURL];
+    console.log(longURL)
+    res.render('pages/urls_show',{shortURL:shortURL, longURL:longURL})
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let shortURL = generateRandomString();
+   urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
-
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
 
 //generates random number for hash generator
 function generateRandomString() {
@@ -54,3 +52,8 @@ String.prototype.hashCode = function() {
   }
   return hash.toString(32);
 };
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
