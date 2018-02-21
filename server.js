7a1a6c05d6184e301563f8b61e770849bed92253
David Haynes <DavidHaynesVictoria@gmail.com>
2018-02-20 18:54:51 -0800

    added the new url page

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+var PORT = process.env.PORT || 8080;
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.end("Hello!");
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render("pages/urls_index", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls/new", (req, res) => {
+  res.render("pages/urls_new");
 });
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {shortURL: urlDatabase};
+  res.render("pages/urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 
